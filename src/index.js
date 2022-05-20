b9ad5f6e59d745f935a7f3b615ffc17b7c0d08f7
Musyaffaq <Musyaffaq@users.noreply.github.com>
2022-05-20 16:58:05 +0800

    added auth

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Amplify from 'aws-amplify';
+import config from './aws-exports'
+Amplify.configure(config);
+// the aws-exports.js file was somewhere else, if there's an error, likelihood it was created in another folder
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
